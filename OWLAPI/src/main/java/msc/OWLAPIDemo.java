8909a703a7bf0ad3400584d84332c8a92aae0c8b
tcrasset <tomcrasset@gmail.com>
2019-05-13 13:53:41 +0200

    Add simple Manchester syntax query

--- OWLAPI/src/main/java/msc/OWLAPIDemo.java
+++ OWLAPI/src/main/java/msc/OWLAPIDemo.java
@@ -2,16 +2,29 @@ package msc;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.Collections;
 import java.util.Optional;
-
+import java.util.Set;
 
 import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.expression.OWLEntityChecker;
+import org.semanticweb.owlapi.expression.ParserException;
+import org.semanticweb.owlapi.expression.ShortFormEntityChecker;
 import org.semanticweb.owlapi.formats.FunctionalSyntaxDocumentFormat;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.reasoner.InferenceType;
+import org.semanticweb.owlapi.reasoner.Node;
+import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.util.ShortFormProvider;
+import org.semanticweb.owlapi.util.SimpleShortFormProvider;
+import org.coode.owlapi.manchesterowlsyntax.ManchesterOWLSyntaxEditorParser;
+import org.semanticweb.owlapi.util.BidirectionalShortFormProvider;
+import org.semanticweb.owlapi.util.BidirectionalShortFormProviderAdapter;
+import org.semanticweb.owlapi.util.ShortFormProvider;
+import org.semanticweb.owlapi.util.SimpleShortFormProvider;
 
 public class OWLAPIDemo {
 	public static void main(String[] args) {
@@ -132,9 +145,190 @@ public class OWLAPIDemo {
 			System.out.println("\nInstances of Sport :");
 			reasoner.getInstances(dataFactory.getOWLClass(IRI.create(oldontologyIRI.get() + "#Sport")),false)
 					.forEach(System.out::println);
+			
+			System.out.println("");
+			System.out.println("========================================================================");
+			System.out.println("===================== MANCHESTER SYNTAX QUERIES==========================");
+			System.out.println("========================================================================");
+			System.out.println("");
+			
+	        ShortFormProvider shortFormProvider = new SimpleShortFormProvider();
+	        // Create the DLQueryPrinter helper class. This will manage the
+	        // parsing of input and printing of results
+	        DLQueryPrinter dlQueryPrinter = new DLQueryPrinter(new DLQueryEngine(reasoner,
+	                shortFormProvider), shortFormProvider);
+	        
+            dlQueryPrinter.askQuery("hasSport some Sport");
+
+			
+			
 		 } catch (Exception e) {
 			 e.printStackTrace();
  		}
 		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class DLQueryEngine {
+    private final OWLReasoner reasoner;
+    private final DLQueryParser parser;
+
+    public DLQueryEngine(OWLReasoner reasoner, ShortFormProvider shortFormProvider) {
+        this.reasoner = reasoner;
+        parser = new DLQueryParser(reasoner.getRootOntology(), shortFormProvider);
+    }
+
+    public Set<OWLClass> getSuperClasses(String classExpressionString, boolean direct) {
+        if (classExpressionString.trim().length() == 0) {
+            return Collections.emptySet();
+        }
+        OWLClassExpression classExpression = parser
+                .parseClassExpression(classExpressionString);
+        NodeSet<OWLClass> superClasses = reasoner
+                .getSuperClasses(classExpression, direct);
+        return superClasses.getFlattened();
+    }
+
+    public Set<OWLClass> getEquivalentClasses(String classExpressionString) {
+        if (classExpressionString.trim().length() == 0) {
+            return Collections.emptySet();
+        }
+        OWLClassExpression classExpression = parser
+                .parseClassExpression(classExpressionString);
+        Node<OWLClass> equivalentClasses = reasoner.getEquivalentClasses(classExpression);
+        Set<OWLClass> result = null;
+        if (classExpression.isAnonymous()) {
+            result = equivalentClasses.getEntities();
+        } else {
+            result = equivalentClasses.getEntitiesMinus(classExpression.asOWLClass());
+        }
+        return result;
+        }
+
+    public Set<OWLClass> getSubClasses(String classExpressionString, boolean direct) {
+        if (classExpressionString.trim().length() == 0) {
+            return Collections.emptySet();
+        }
+        OWLClassExpression classExpression = parser
+                .parseClassExpression(classExpressionString);
+        NodeSet<OWLClass> subClasses = reasoner.getSubClasses(classExpression, direct);
+        return subClasses.getFlattened();
+        }
+
+    public Set<OWLNamedIndividual> getInstances(String classExpressionString,
+            boolean direct) {
+        if (classExpressionString.trim().length() == 0) {
+            return Collections.emptySet();
+        }
+        OWLClassExpression classExpression = parser
+                .parseClassExpression(classExpressionString);
+        NodeSet<OWLNamedIndividual> individuals = reasoner.getInstances(classExpression,
+                direct);
+        return individuals.getFlattened();
+        }
+    }
+
+class DLQueryParser {
+    private final OWLOntology rootOntology;
+    private final BidirectionalShortFormProvider bidiShortFormProvider;
+
+    public DLQueryParser(OWLOntology rootOntology, ShortFormProvider shortFormProvider) {
+        this.rootOntology = rootOntology;
+        OWLOntologyManager manager = rootOntology.getOWLOntologyManager();
+        Set<OWLOntology> importsClosure = rootOntology.getImportsClosure();
+        // Create a bidirectional short form provider to do the actual mapping.
+        // It will generate names using the input
+        // short form provider.
+        bidiShortFormProvider = new BidirectionalShortFormProviderAdapter(manager,
+                importsClosure, shortFormProvider);
+    }
+
+    public OWLClassExpression parseClassExpression(String classExpressionString) {
+        OWLDataFactory dataFactory = rootOntology.getOWLOntologyManager()
+                .getOWLDataFactory();
+        ManchesterOWLSyntaxEditorParser parser = new ManchesterOWLSyntaxEditorParser(
+                dataFactory, classExpressionString);
+        parser.setDefaultOntology(rootOntology);
+        OWLEntityChecker entityChecker = new ShortFormEntityChecker(bidiShortFormProvider);
+        parser.setOWLEntityChecker(entityChecker);
+        return parser.parseClassExpression();
+        }
+    }
+
+class DLQueryPrinter {
+    private final DLQueryEngine dlQueryEngine;
+    private final ShortFormProvider shortFormProvider;
+
+    public DLQueryPrinter(DLQueryEngine engine, ShortFormProvider shortFormProvider) {
+        this.shortFormProvider = shortFormProvider;
+        dlQueryEngine = engine;
+        }
+
+    public void askQuery(String classExpression) {
+        if (classExpression.length() == 0) {
+            System.out.println("No class expression specified");
+        } else {
+            try {
+                StringBuilder sb = new StringBuilder();
+                sb.append("\nQUERY:   ").append(classExpression).append("\n\n");
+                Set<OWLClass> superClasses = dlQueryEngine.getSuperClasses(
+                        classExpression, false);
+                printEntities("SuperClasses", superClasses, sb);
+                Set<OWLClass> equivalentClasses = dlQueryEngine
+                        .getEquivalentClasses(classExpression);
+                printEntities("EquivalentClasses", equivalentClasses, sb);
+                Set<OWLClass> subClasses = dlQueryEngine.getSubClasses(classExpression,
+                        true);
+                printEntities("SubClasses", subClasses, sb);
+                Set<OWLNamedIndividual> individuals = dlQueryEngine.getInstances(
+                        classExpression, true);
+                printEntities("Instances", individuals, sb);
+                System.out.println(sb.toString());
+            } catch (ParserException e) {
+                System.out.println(e.getMessage());
+            }
+            }
+        }
+
+    private void printEntities(String name, Set<? extends OWLEntity> entities,
+            StringBuilder sb) {
+        sb.append(name);
+        int length = 50 - name.length();
+        for (int i = 0; i < length; i++) {
+            sb.append(".");
+        }
+        sb.append("\n\n");
+        if (!entities.isEmpty()) {
+            for (OWLEntity entity : entities) {
+                sb.append("\t").append(shortFormProvider.getShortForm(entity))
+                        .append("\n");
+            }
+        } else {
+            sb.append("\t[NONE]\n");
+            }
+        sb.append("\n");
+        }
+    }	
